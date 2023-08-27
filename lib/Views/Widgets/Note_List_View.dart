4c49773a86cfeecbdd4b91ed4333e5d6d7c52bdd
osamamahmoud2 <osama255955@gmail.com>
2023-08-27 14:29:14 -0700

    Finishing Ui

---
+++ lib/Views/Widgets/Note_List_View.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+import 'Notes_Item.dart';
+
+class NotesListView extends StatelessWidget {
+  const NotesListView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 16),
+      child: ListView.builder(
+        padding: EdgeInsets.zero,
+        itemBuilder: ((context, index) {
+          return const Padding(
+            padding: EdgeInsets.symmetric(vertical: 4),
+            child: NoteItem(),
+          );
+        }),
+      ),
+    );
+  }
+}
